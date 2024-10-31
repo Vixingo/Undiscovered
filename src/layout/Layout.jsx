28abdd92857caaf589cab9ec3f8deb2596126a17
Vixingo <nazmul.abbu@gmail.com>
2024-10-31 22:32:03 +0800

    as

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -32,6 +32,7 @@ const Layout = () => {
                 {pathname === "/" ||
                 pathname === "/Playerinfo" ||
                 pathname === "/about" ||
+                pathname === "/newsfeed" ||
                 pathname === "/Coachinfo" ? (
                     <Box
                         sx={{
